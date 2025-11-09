13412e46846147f296379be645151c4f21a89c57
Vinícius Reis <viniciusreis3105@gmail.com>
2025-11-09 12:04:32 -0300

    feat: add data db for catalogo cards

---
+++ src/data/catalogo.ts
@@ -0,0 +1,64 @@
+export const catalogoItems = [
+  {
+    title: "Cartão de Visita",
+    description:
+      "Cartão de visita é uma forma de apresentar sua empresa ou marca de forma rápida e eficaz. ",
+    imageSrc: "/cartao.avif",
+  },
+  {
+    title: "Panfletos",
+    description:
+      "Panfletos são folhetos de papel que podem ser distribuídos para divulgar informações sobre uma empresa ou produto.",
+    imageSrc: "/panfletos.avif",
+  },
+  {
+    title: "Blocos de Papel",
+    description: "",
+    imageSrc: "/bloco.avif",
+  },
+  {
+    title: "Lacres e Adesivos",
+    description: "",
+    imageSrc: "/lacre.avif",
+  },
+  {
+    title: "Adesivos em Geral",
+    description: "",
+    imageSrc: "/adesivos.avif",
+  },
+  {
+    title: "Fachadas",
+    description: "",
+    imageSrc: "/fachada.avif",
+  },
+  {
+    title: "Poster",
+    description: "",
+    imageSrc: "/poster.avif",
+  },
+  {
+    title: "Tags",
+    description: "",
+    imageSrc: "/tag.avif",
+  },
+  {
+    title: "Placas",
+    description: "",
+    imageSrc: "/placas.avif",
+  },
+  {
+    title: "Plotagem",
+    description: "",
+    imageSrc: "/plotagem.avif",
+  },
+  {
+    title: "Aplicação de Películas",
+    description: "",
+    imageSrc: "/pelicula.avif",
+  },
+  {
+    title: "Criação de Artes Gráficas",
+    description: "",
+    imageSrc: "/artes.avif",
+  },
+];
