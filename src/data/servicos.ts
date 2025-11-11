6b9d5696af95183282f872696f154f573b4dd243
Vinícius Reis <viniciusreis3105@gmail.com>
2025-11-10 21:52:55 -0300

    feat: add services cards reusables

---
+++ src/data/servicos.ts
@@ -0,0 +1,28 @@
+export const services = [
+  {
+    title: "Impressão Offset",
+    description:
+      "Oferecemos uma ampla variedade de serviços de impressão para ajudar sua empresa a se comunicar de maneira clara e eficaz com seus clientes. Se você precisa de grandes tiragens temos a solução ideal para você",
+    services: [
+      "Cartão de Visita",
+      "Panfletos",
+      "Blocos",
+      "Embalagem Personalizadas",
+      "Cartaz",
+    ],
+  },
+
+  {
+    title: "Impressão Digital",
+    description:
+      "Oferecemos serviços de impressão digital de pequeno e grande formato, para ajudar sua empresa a se destacar no mercado.",
+    services: [
+      "Wind Banner/Banner",
+      "Fachadas",
+      "Placas",
+      "Adesivos em Geral",
+      "Embalagens Personalizadas",
+      "Etiquetas e Tags",
+    ],
+  },
+];
